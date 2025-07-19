20df0f7740d5db696ebc77fec1a30c29c3b43db6
slam <slamhan1987@gmail.com>
2025-07-19 11:56:38 +0800

    fix typo in app name formatting function

--- ProjectDescriptionHelpers/BundleID.swift
+++ ProjectDescriptionHelpers/BundleID.swift
@@ -9,7 +9,7 @@
 // MARK: - Bundle IDs
 
 public enum BundleID {
-    public static func app(_ appName: String, base: String) -> String { "\(base).\(appNam.lowercased())" }
+    public static func app(_ appName: String, base: String) -> String { "\(base).\(appName.lowercased())" }
     public static func appTests(_ appName: String, base: String) -> String { "\(base).\(appName.lowercased())Tests" }
     public static func module(_ moduleName: String, base: String) -> String { "\(base).\(moduleName.lowercased())" }
     public static func moduleTests(_ moduleName: String, base: String) -> String { "\(base).\(moduleName.lowercased())Tests" }
