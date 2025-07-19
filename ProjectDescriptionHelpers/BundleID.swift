c4248d3e986ad99087d5ce11cbdefbcd781618b0
slam <slamhan1987@gmail.com>
2025-07-19 11:53:29 +0800

    normalize app and module naming by converting to lowercase

--- ProjectDescriptionHelpers/BundleID.swift
+++ ProjectDescriptionHelpers/BundleID.swift
@@ -9,8 +9,8 @@
 // MARK: - Bundle IDs
 
 public enum BundleID {
-    public static func app(_ appName: String, base: String) -> String { "\(base).\(appName)" }
-    public static func appTests(_ appName: String, base: String) -> String { "\(base).\(appName)Tests" }
-    public static func module(_ moduleName: String, base: String) -> String { "\(base).\(moduleName)" }
-    public static func moduleTests(_ moduleName: String, base: String) -> String { "\(base).\(moduleName)Tests" }
+    public static func app(_ appName: String, base: String) -> String { "\(base).\(appNam.lowercased())" }
+    public static func appTests(_ appName: String, base: String) -> String { "\(base).\(appName.lowercased())Tests" }
+    public static func module(_ moduleName: String, base: String) -> String { "\(base).\(moduleName.lowercased())" }
+    public static func moduleTests(_ moduleName: String, base: String) -> String { "\(base).\(moduleName.lowercased())Tests" }
 }
